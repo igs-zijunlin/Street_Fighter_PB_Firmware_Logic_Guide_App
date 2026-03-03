60dca4ad14b7e9bfaca361e73256500362941578
zijunlin <zijunlin@igs.com.tw>
2026-03-03 09:49:17 +0800

    feat(action)上傳actions需要的yml檔案

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Street_Fighter_PB_Firmware_Logic_Guide_App/',
   plugins: [
     react(),
     tailwindcss(),
